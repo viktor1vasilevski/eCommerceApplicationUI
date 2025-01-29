5bfa9256d7acd34c1663fe03898dee9bbe42bd93
vik_vas <vasilevskiviktor@yahoo.com>
2025-01-29 14:07:39 +0100

    add categories init logic

--- src/app/admin/pages/categories/categories.component.ts
+++ src/app/admin/pages/categories/categories.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
+
+  isEditMode: boolean = false;
+  categories: any[] = [];
+  category: any;
+
+  onSubmit() {
+
+  }
+
+  onEdit(cat: any) {
+
+  }
+
+  onDelete(cat: any) {
+
+  }
+
+  cancelEdit() {
+
+  }
 
 }
