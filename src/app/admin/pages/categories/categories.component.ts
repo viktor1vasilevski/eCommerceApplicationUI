ff90cdd7968f516b7ab3c8aba9c029be0aa4b341
vik_vas <vasilevskiviktor@yahoo.com>
2025-02-01 17:46:43 +0100

    code cleanup

--- src/app/admin/pages/categories/categories.component.ts
+++ src/app/admin/pages/categories/categories.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CategoryService } from '../../../shared/services/category.service';
 import { CategoryRequest } from '../../../shared/models/category/category-request';
 import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
@@ -8,8 +8,6 @@ import { SortOrder } from '../../../core/enums/sort-order.enum';
 import { ErrorHandlerService } from '../../../core/services/error-handler.service';
 import { NotificationService } from '../../../core/services/notification.service';
 import { ApiResponse } from '../../../core/models/responses/api-response';
-import { CreateCategoryDTO } from '../../models/category/create-category-dto';
-import { EditCategoryDTO } from '../../models/category/edit-category-dto';
 import { CategoryDTO } from '../../models/category/category-dto';
 import { PaginationComponent } from "../../components/pagination/pagination.component";
 import { NonGenericApiResponse } from '../../../core/models/responses/non-generic-api-response';
@@ -25,18 +23,13 @@ declare var bootstrap: any;
 })
 export class CategoriesComponent implements OnInit {
 
-  isEditMode: boolean = false;
   totalCount: number = 0;
   totalPages: number[] = [];
   currentPage: number = 1;
-
   categories: any;
-  selectedCategory: any;
-  categoryToDelete: any = null; // Holds the category to delete
+  categoryToDelete: any = null;
+
   @ViewChild('categoryNameInput') categoryNameInput!: ElementRef;
-
-  createEditCategoryForm: FormGroup;
-
   private nameChangeSubject = new Subject<string>();
 
   categoryRequest: CategoryRequest = {
@@ -47,15 +40,12 @@ export class CategoriesComponent implements OnInit {
   };
 
   constructor(
-    private fb: FormBuilder,
     private _categoryService: CategoryService,
     private _errorHandlerService: ErrorHandlerService,
     private _notificationService: NotificationService,
     private router: Router
   ) {
-    this.createEditCategoryForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-    });
+
   }
 
   onNameChange(): void {
@@ -76,13 +66,6 @@ export class CategoriesComponent implements OnInit {
       });
   }
 
-  onItemsPerPageChange(itemsPerPage: number): void {
-    this.categoryRequest.take = itemsPerPage;
-    this.categoryRequest.skip = 0;
-    this.currentPage = 1;
-    this.loadCategories();
-  }
-
   loadCategories() {
     this._categoryService.getCategories(this.categoryRequest).subscribe({
       next: (response: ApiResponse<CategoryDTO[]>) => {
@@ -100,10 +83,17 @@ export class CategoriesComponent implements OnInit {
     });
   }
 
+  onItemsPerPageChange(itemsPerPage: number): void {
+    this.categoryRequest.take = itemsPerPage;
+    this.categoryRequest.skip = 0;
+    this.currentPage = 1;
+    this.loadCategories();
+  }
+
   changePage(page: number): void {
     this.currentPage = page;
     this.categoryRequest.skip = (page - 1) * this.categoryRequest.take;
-    this.loadCategories(); // Fetch data for the new page
+    this.loadCategories();
   }
 
   toggleSortOrder() {
@@ -115,61 +105,6 @@ export class CategoriesComponent implements OnInit {
     const pages = Math.ceil(this.totalCount / this.categoryRequest.take);
     this.totalPages = Array.from({ length: pages }, (_, i) => i + 1);
   }
-
-  onCreateEditCategory() {
-    if (!this.createEditCategoryForm.valid) {
-      this._notificationService.info("Invalid form");
-      return;
-    }
-    const createEditCategoryForm = this.createEditCategoryForm.value;
-    this.isEditMode ? this.editCategory(createEditCategoryForm) : this.createCategory(createEditCategoryForm);
-  }
-
-  editCategory(form: any) {
-    this._categoryService.editCategory(this.selectedCategory.id, form).subscribe({
-      next: (response: ApiResponse<EditCategoryDTO>) => {
-        if (response && response.success) {
-          this.loadCategories();
-          this._notificationService.success(response.message);
-          this.createEditCategoryForm.reset();
-          this.isEditMode = false;
-        } else {
-          this._notificationService.info(response.message);
-        }
-      },
-      error: (errorResponse: ApiResponse<EditCategoryDTO>) => {
-        this._errorHandlerService.handleErrors(errorResponse);
-      }
-    });
-  }
-
-  createCategory(form: any) {
-    this._categoryService.createCategory(form).subscribe({
-      next: (response: ApiResponse<CreateCategoryDTO>) => {
-        if (response && response.success) {
-          this.loadCategories();
-          this._notificationService.success(response.message);
-          this.createEditCategoryForm.reset();
-        } else {
-          this._notificationService.info(response.message);
-        }
-      },
-      error: (errorResponse: ApiResponse<CreateCategoryDTO>) => {
-        this._errorHandlerService.handleErrors(errorResponse);
-      }
-    });
-  }
-
-  // prepareForEdit(category: any): void {
-  //   this.isEditMode = true;
-  //   this.selectedCategory = category;
-  //   this.createEditCategoryForm.patchValue({
-  //     name: category.name
-  //   });
-  //   setTimeout(() => this.categoryNameInput?.nativeElement.focus(), 0);
-  // }
-
-
 
   prepareForDelete(category: any): void {
     this.categoryToDelete = category;
@@ -211,16 +146,6 @@ export class CategoriesComponent implements OnInit {
     this.categoryToDelete = null;
   }
 
-  cancelEdit() {
-    this.isEditMode = false;
-    this.createEditCategoryForm.reset();
-  }
-
-  ngOnDestroy(): void {
-    this.nameChangeSubject.complete();
-  }
-
-
   loadCreateCategoryPage() {
     this.router.navigate(['/admin/categories/create'])
     
@@ -228,5 +153,10 @@ export class CategoriesComponent implements OnInit {
 
   loadEditCategoryPage(categoryId: string) {
     this.router.navigate([`/admin/categories/edit/${categoryId}`]);
+  }
+
+
+  ngOnDestroy(): void {
+    this.nameChangeSubject.complete();
   }
 }
